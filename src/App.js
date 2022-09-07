08f2bf3f8be54142d24721f5b92078688e1ee9a4
Gayathri D R <99469991+gayathri257developer@users.noreply.github.com>
2022-09-07 16:48:11 +0530

    Update App.js
    recode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import './App.css';
 import React from 'react'
-import FeedbackForm from './Components/FeedbackForm';
+import FeedbackForm from './Components/FeedbackForm'
 
-class Form extends React.Component{
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name: '',
+      depart: '',
+      rating:'',
+      users:[]
+    }
+  }
 
- render(){
- return(
-<div>
- <FeedbackForm/>
-</div>
- )
+  updateName = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  updateDepartment = (event) => {
+    this.setState({depart: event.target.value});
+  }
+
+  updateRating = (event) => {  
+    this.setState({rating: event.target.value});
+  }
+
+  handleOnClick  = () => {
+    const updatedUsers = this.state.users;
+
+    updatedUsers.push({name:this.state.name, depart:this.state.depart, rating:this.state.rating})
+    this.setState({users:updatedUsers, name:'', depart:'', rating:'' })
+  }
+
+  render() {
+    return (
+      
+      <div>
+      <button onClick={this.handleOnClick}> Submit </button>
+
+        <FeedbackForm name={this.state.name} depart={this.state.depart} rating={this.state.rating} users={this.state.users} 
+        updateName={this.updateName} updateDepartment={this.updateDepartment} updateRating={this.updateRating}/>
+        
+      </div>
+    
+    )
+  }
 }
-}
-export default Form
+
+export default App;
